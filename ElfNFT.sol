3978d9beb6433b2de181b40b4d5534b37e43aa25
elfmatrix <98001453+elfmatrix@users.noreply.github.com>
2022-02-08 09:42:18 +0800

    Update ElfNFT.sol

--- ElfNFT.sol
+++ ElfNFT.sol
@@ -5,7 +5,7 @@ pragma abicoder v2;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
-import "AccessProtected.sol";
+import "./AccessProtected.sol";
 
 contract ElfNFT is ERC721URIStorage, AccessProtected {
     using Counters for Counters.Counter;
